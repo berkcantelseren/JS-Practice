1c11db69c566316e0b8bdb22a4a40dcbeeace8a5
berkcantelseren <106893348+berkcantelseren@users.noreply.github.com>
2025-01-10 21:57:41 +0300

    DOM Lesson
    DOM

---
+++ lesson-07/index.js
@@ -0,0 +1,21 @@
+console.log(document.querySelector("button").innerHTML);
+document.querySelector("button").innerHTML = "Changed";
+
+const buttonElement = document.querySelector(".js-button");
+console.log(buttonElement);
+
+/*
+      console.log(document.title);
+      document.title = 'Changed';
+
+      console.log(document.body);
+      console.log(typeof document.body);
+
+      console.log(document.body.innerHTML);
+      document.body.innerHTML = '<button>Good job!</button>';
+      */
+
+/*
+      document.body.innerHTML = 'hello';
+      document.title = 'Good job!';
+      */
